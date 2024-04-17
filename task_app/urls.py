73cfbb00b11271dbb4b7c7a58b139a1d2706eade
Akyl <akyl@gmail.com>
2024-04-18 00:41:11 +0200

    add registration

--- task_app/urls.py
+++ task_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('register/', views.register, name='register'),
+    path('logout', views.logout_view, name='logout2'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- task_app/views.py
+++ task_app/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login, logout
 # Create your views here.
 
 def index(request):
-    return HttpResponse('test')
+    return render(request, 'task_app/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('home')
+
+    form = NewUserForm
+    return render(request, 'registration/register.html', {'form':form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
